fec787842297ebe551c6fe62a76fb95c78d9aedd
张志伟 <9539266@qq.com>
2017-08-21 11:25:07 +0800

    1

---
+++ WeChatPro/Controllers/WeChatController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Senparc.Weixin.MP.Entities.Request;
+using Senparc.Weixin.MP.MvcExtension;
+using Senparc.Weixin.MP;
+//using Apps.Web.Areas.WC.Core;
+
+namespace Apps.Web.Areas.WC.Controllers
+{
+    public class WeChatController : Controller
+    {
+        public static readonly string Token = "WeixinToken";//与微信公众账号后台的Token设置保持一致，区分大小写。
+        public static readonly string EncodingAESKey = "dEq1BjMgmkEyOvva8pQfFwX95hBLOYKpAzBJ5y9pdSK";//与微信公众账号后台的EncodingAESKey设置保持一致，区分大小写。
+        public static readonly string AppId = "wx3.......f5";//与微信公众账号后台的AppId设置保持一致，区分大小写。
+
+        // GET: WC/WeChat
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [ActionName("Index")]
+        public Task<ActionResult> Get(string signature, string timestamp, string nonce, string echostr)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                if (CheckSignature.Check(signature, timestamp, nonce, Token))
+                {
+                    return echostr; //返回随机字符串则表示验证通过
+                }
+                else
+                {
+                    return "failed:" + signature + "," + CheckSignature.GetSignature(timestamp, nonce, Token) + "。" +
+                        "如果你在浏览器中看到这句话，说明此地址可以被作为微信公众账号后台的Url，请注意保持Token一致。";
+                }
+            }).ContinueWith<ActionResult>(task => Content(task.Result));
+        }
+
+
+        /// <summary>
+        /// 最简化的处理流程
+        /// </summary>
+        [HttpPost]
+        [ActionName("Index")]
+        public Task<ActionResult> Post(PostModel postModel)
+        {
+            return Task.Factory.StartNew<ActionResult>(() =>
+            {
+                if (!CheckSignature.Check(postModel.Signature, postModel.Timestamp, postModel.Nonce, Token))
+                {
+                    return new WeixinResult("参数错误！");
+                }
+
+                postModel.Token = Token;
+                postModel.EncodingAESKey = EncodingAESKey; //根据自己后台的设置保持一致
+                postModel.AppId = AppId; //根据自己后台的设置保持一致
+
+                //var messageHandler = new CustomMessageHandler(Request.InputStream, postModel, 10);
+
+                //messageHandler.Execute(); //执行微信处理过程
+
+                //return new FixWeixinBugWeixinResult(messageHandler);
+                return new WeixinResult("参数错误！");
+
+            }).ContinueWith<ActionResult>(task => task.Result);
+        }
+
+    }
+}
